4422ab03193da646da18d2440f04f2ce2b97b1e4
Andreas Källberg <andreas@svalna.se>
2020-07-16 13:49:40 +0200

    Rename node

--- SearchTree.hs
+++ SearchTree.hs
@@ -17,7 +17,7 @@ data Nat = Z | S Nat
 
 data SearchTree leftBound rightBound a where
   Leaf :: SearchTree leftBound rightBound a
-  Node' ::
+  Node ::
     SearchTree leftBound la a ->
     Smaller leftBound la ->
     Named la a ->
@@ -29,23 +29,23 @@ deriving instance Show a => Show (SearchTree lb rb a)
 
 type UnboundedTree = SearchTree NegInfty Infinity
 
-pattern Node a lb rb l r = Node' l lb a rb r
--- retrie --adhoc "forall x lb rb l r. Node x lb rb l r = Node' l lb x rb r" -i2 --target-file SearchTree.hs
+pattern Nyde a lb rb l r = Node l lb a rb r
+-- retrie --adhoc "forall x lb rb l r. Nyde x lb rb l r = Node l lb x rb r" -i2 --target-file SearchTree.hs
 
 insert :: Ord a => a -> UnboundedTree a -> UnboundedTree a
 insert x = name x \nx -> insert' nx cmpNegInfty cmpInfinity
--- insert x Leaf = name x $ \nx -> Node nx cmpNegInfty cmpInfinity Leaf Leaf
+-- insert x Leaf = name x $ \nx -> Nyde nx cmpNegInfty cmpInfinity Leaf Leaf
 
 insert' :: Ord a => Named l a -> Smaller lb l -> Smaller l rb -> SearchTree lb rb a -> SearchTree lb rb a
-insert' x lb rb Leaf = Node' Leaf lb x rb Leaf
-insert' x lb rb (Node y lb' rb' l r) = case compareNamed x y of
-    Left ans -> Node' (insert' x lb ans l) lb' y rb' r
-    Right ans -> Node' l lb' y rb' (insert' x ans rb r)
+insert' x lb rb Leaf = Node Leaf lb x rb Leaf
+insert' x lb rb (Nyde y lb' rb' l r) = case compareNamed x y of
+    Left ans -> Node (insert' x lb ans l) lb' y rb' r
+    Right ans -> Node l lb' y rb' (insert' x ans rb r)
 
 
 -- rotate
 rotateR :: SearchTree lb rb a -> SearchTree lb rb a
-rotateR (Node x _lb rb (Node y llb lrb ll lr) r) = Node' ll llb y (cmpTrans lrb rb) (Node' lr lrb x rb r)
+rotateR (Nyde x _lb rb (Nyde y llb lrb ll lr) r) = Node ll llb y (cmpTrans lrb rb) (Node lr lrb x rb r)
 rotateR x = x
 
 
